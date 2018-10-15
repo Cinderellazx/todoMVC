676238803e3489b05834b9f196f558ecfe53df39
Cinderellazx <cinderella_bj@sina.com>
2018-10-15 20:21:47 +0800

    完成页面渲染

--- js/app.js
+++ js/app.js
@@ -1,6 +1,33 @@
 (function (window) {
-	'use strict';
+	
+	const ajax = axios.create({
+		baseURL: 'http://localhost:8080/todos'
+	});
 
-	// Your starting point. Enjoy the ride!
+	getData();
 
+	// 获取数据
+	function getData() {
+		ajax.get('/getDataAll').then(res => {
+			const {data, meta} = res.data;
+			if(meta.code === 200) {
+			// console.log(res.data);
+			bindList(data);
+			}
+		});
+	};
+	// 渲染页面
+
+	function bindList(arr) {
+		// console.log('渲染页面');
+		const Activearr = arr.filter(item => {
+			return item.isFinish === '0';
+		}).length;
+		const todoHtml = template('todoTpl',{list: arr, num: Activearr});
+		const todoapp = document.querySelector('.todoapp');
+		todoapp.innerHTML = todoHtml;
+	};
+
+	
+	
 })(window);
